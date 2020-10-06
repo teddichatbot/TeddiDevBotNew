c9fcea2d109ab1e502dab03f042a63ae11e00122
subhankarCN <subhankar.ray@capitalnumbers.com>
2020-10-06 11:25:17 +0530

    live api issue fixed

--- bot.js
+++ bot.js
@@ -253,7 +253,7 @@ async function UtteranceLog(storage, turnContext, userId, storeItems, channelId,
 async function creatingUtterance(storage, turnContext, userId, storeItems, channelId, conversationId, userChatId, chapterType){
     let dateNow = new Date();
     let botReply = ''; 
-    console.log('MicrosoftAppId',process.env.MicrosoftAppId)
+    // console.log('MicrosoftAppId', process.env.MicrosoftAppId)
     botReply = chatJson['introduction']['intro'][1]['text']
 
     let botResp = await turnContext.sendActivities([
--- dbConfig.js
+++ dbConfig.js
@@ -1,11 +1,12 @@
+require('dotenv').config()
 var dbConfig = {};
-
-dbConfig.API_URL = "https://teddinodeapp.azurewebsites.net/"; //API url (teddi backend)
-dbConfig.DB_SERVICE_ENDPOINT = "https://teddi-cosmosdb-instance-sql.documents.azure.com:443/"; //cosmos db url
-dbConfig.AUTH_KEY = "FAtkho7iQAq7CvuArBfpOb9MD1ByFCVPEIkUpRp8NCtN3N0cGptD8xANihUYYpZZJf4McyQfOCCfCIdjZMdgYQ=="; //cosmos b auth key
+dbConfig.ENV_MOOD = (process.env.MicrosoftAppId == '1ab3d967-8d07-4c6f-b697-5ab3a2a04690') ? 'DEV':'LIVE';
+dbConfig.API_URL = (dbConfig.ENV_MOOD == 'DEV') ? "https://teddinodeapp.azurewebsites.net/" : "https://teddibackend.azurewebsites.net/"; //API url (teddi backend)
+dbConfig.DB_SERVICE_ENDPOINT = (dbConfig.ENV_MOOD == 'DEV') ? "https://teddi-cosmosdb-instance-sql.documents.azure.com:443/" : "https://teddi-db.documents.azure.com:443/"; //cosmos db url
+dbConfig.AUTH_KEY = (dbConfig.ENV_MOOD == 'DEV') ? "FAtkho7iQAq7CvuArBfpOb9MD1ByFCVPEIkUpRp8NCtN3N0cGptD8xANihUYYpZZJf4McyQfOCCfCIdjZMdgYQ==": "QywTQMRPKGGROlyIxmdAxvm4uWumPPbeGjGzbgsFg4710K1pLapMcBn5woKGU1k7YTXn1GGYaXtBNSXpCZPoEw=="; //cosmos b auth key
 dbConfig.DATABASE_ID = "botdocs";
 dbConfig.CONTAINER = "botdata";
 
-
+// console.log(dbConfig)
 
 module.exports = dbConfig;
